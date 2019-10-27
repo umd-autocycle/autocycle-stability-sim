26914f274e834d0baa0884bf4cd34f8e76823fe9
Evan Ruderman <evanrman@comcast.net>
2019-10-27 12:24:07 -0400

    arbitrary legends added

--- graphs.py
+++ graphs.py
@@ -5,8 +5,8 @@ import numpy as np
 def plot_params(xtuple, *data):
     for datum in data:
         plt.plot(xtuple[0], datum[0])
+    plt.legend([datum[1] for datum in data])
     plt.xlabel(xtuple[1])
-    plt.ylabel(datum[1])
     plt.show()
 
 
@@ -18,7 +18,9 @@ time = np.arange(0, 10, .01)
 timeName = "time"
 data = f(time)
 dataName = "roll"
+dataaa = 2*f(time)
+data2 = (dataaa,"other")
 xaxis = (time, timeName)
 yaxis = (data, dataName)
 
-plot_params(xaxis, yaxis)
+plot_params(xaxis, yaxis, data2)
