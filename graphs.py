a6a1fcbb2873e9a8f30070abfcb0af54bff8e828
Evan Ruderman <evanrman@comcast.net>
2019-10-27 13:36:23 -0400

    test code moved to separate file. font sizes/types/other general visuals added to graphs
    will later be able to integrate with tkinter gui

--- graphs.py
+++ graphs.py
@@ -1,26 +1,29 @@
 import matplotlib.pyplot as plt
+import matplotlib
 import numpy as np
 
+def plot_params(title, xtuple, *data, dependent=""):
+    matplotlib.rcParams['lines.linewidth'] = 2.5
+    matplotlib.rc('font',family='Arial')
 
-def plot_params(xtuple, *data):
+
     for datum in data:
         plt.plot(xtuple[0], datum[0])
-    plt.legend([datum[1] for datum in data])
-    plt.xlabel(xtuple[1])
+    plt.legend([datum[1] for datum in data],fontsize = 14)
+    plt.xlabel(xtuple[1],fontsize = 14)
+    plt.ylabel(dependent, fontsize = 14)
+    plt.title(title, fontsize = 20)
+
+    ax = plt.subplot(111)
+    ax.spines["top"].set_visible(False)
+    ax.spines["bottom"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    ax.spines["left"].set_visible(False)
+
+    ax.get_xaxis().tick_bottom()
+    ax.get_yaxis().tick_left()
+    plt.yticks(fontsize=12)
+    plt.xticks(fontsize=12)
+
+
     plt.show()
-
-
-def f(t):
-    return np.exp(-t) * np.cos(2*np.pi*t)
-
-
-time = np.arange(0, 10, .01)
-timeName = "time"
-data = f(time)
-dataName = "roll"
-dataaa = 2*f(time)
-data2 = (dataaa,"other")
-xaxis = (time, timeName)
-yaxis = (data, dataName)
-
-plot_params(xaxis, yaxis, data2)
