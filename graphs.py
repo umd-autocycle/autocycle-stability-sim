61f6c4914060e7a49ed3ad77870de49fabd5663a
Evan Ruderman <evanrman@comcast.net>
2019-10-27 12:05:25 -0400

    Basic graph function plots x-axis name and data and arbitrary y-axis dependent variables in 2D.

---
+++ graphs.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_params(xtuple, *data):
+    for datum in data:
+        plt.plot(xtuple[0], datum[0])
+    plt.xlabel(xtuple[1])
+    plt.ylabel(datum[1])
+    plt.show()
+
+
+def f(t):
+    return np.exp(-t) * np.cos(2*np.pi*t)
+
+
+time = np.arange(0, 10, .01)
+timeName = "time"
+data = f(time)
+dataName = "roll"
+xaxis = (time, timeName)
+yaxis = (data, dataName)
+
+plot_params(xaxis, yaxis)
