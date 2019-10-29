4d720fefea486b38e6ce77823d470149a6b3fe7e
Jack Mirenzi <jmirenzi@umd.edu>
2019-10-28 20:01:58 -0400

    ode solver

--- simulation.py
+++ simulation.py
@@ -8,14 +8,26 @@ M = np.array([[1, 2], [3, 4]])
 C = np.array([[1, 2], [3, 4]])
 K = np.array([[1, 2], [3, 4]])
 
+
+
+from scipy.integrate import solve_ivp
+def rhs(s, v):
+    return [-12*v[2]**2, 12*v[2]**2, 6*v[0]*v[2] - 6*v[2]*v[1] - 36*v[2]]
+res = solve_ivp(rhs, (0, 0.1), [2, 3, 4])
+print(res.t)
+print(res.y)
+'''
 t = sp.symbols('t')
 y0 = Function('y0')(t)
 y1 = Function('y1')(t)
 y = np.array([y0, y1])
+dy = np.array([y0.diff(t), y1.diff(t)])
+d2y = np.array([y0.diff(t,2), y1.diff(t,2)])
 
-diffeq = Eq(M*y.diff(t,2))
+diffeq = M*d2y+C*dy+K*y
+print(dsolve(Eq(diffeq[0]),)
 
-'''
+
 x = sp.Function('x')(t)
 y = sp.Function('y')(t)
 diffeq1 = Eq(x.diff(t, 2) + y.diff(t, 2) + x.diff(t) + y.diff(t) + x + y, 0)
