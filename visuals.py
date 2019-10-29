6923ca57eb0530cbbe4691ffa58d3de595d13f76
Cooper Grill <coopergrill@gmail.com>
2019-10-29 12:46:13 -0400

    add file

--- visuals.py
+++ visuals.py
@@ -1,8 +1,11 @@
+from math import pi, sin, cos
+
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 from direct.actor.Actor import Actor
 from direct.interval.IntervalGlobal import Sequence
 from panda3d.core import Point3
+from panda3d.core import GeoMipTerrain
 
 class MyApp(ShowBase):
     def __init__(self):
@@ -16,30 +19,27 @@ class MyApp(ShowBase):
         self.scene.setScale(0.25, 0.25, 0.25)
         self.scene.setPos(-8, 42, 0)
 
-        #load and transform panda actor
-        self.pandaActor=Actor("models/panda-model", {"walk": "models/panda-walk4"})
-        self.pandaActor.setScale(0.005, 0.005, 0.005)
+        # load and transform panda actor
+        self.pandaActor = Actor("models/panda-model", {"walk": "models/panda-walk4"})
+        self.pandaActor.setScale(0.00005, 0.0005, 0.0005)
         self.pandaActor.reparentTo(self.render)
-        #loop animation
+        # loop animation
         self.pandaActor.loop("walk")
 
-        #create four lerp intervals needed for panda to walk back and forth
-        pandaPosInterval1=self.pandaActor.posInterval(13, Point3(0, -10, 0), startPos=Point3(0, 10, 0))
-        pandaPosInterval2=self.pandaActor.posInterval(13, Point3(0, 10, 0), startPos=Point3(0, -10, 0))
-        pandaHprInterval1=self.pandaActor.hprInterval(3, Point3(180, 0, 0), startHpr=Point3(0, 0, 0))
-        pandaHprInterval2=self.pandaActor.hprInterval(3, Point3(0, 0, 0), startHpr=Point3(180, 0, 0))
+        # create intervals needed for panda to walk
+        self.pandaActor.setHpr(180, 0, 0)
+        self.pandaActor.setPos(0, -10, 3)
+        self.taskMgr.add(self.pandaTask, "PandaTask")
 
-        #create and play sequence that coordinates intervals
-        self.pandaPace=Sequence(pandaPosInterval1, pandaHprInterval1, pandaPosInterval2, pandaHprInterval2, name="pandaPace")
-        self.pandaPace.loop()
 
-        # add camera procedure to task manager
-        self.taskMgr.add(self.cameraTask, "CameraTask")
-
-    #define procedure to move camera
-    def cameraTask(self, task):
-        self.camera.setPos(self.pandaActor.getPos()-(0, 10, -5))
+    #define procedure to move panda
+    def pandaTask(self, task):
+        angleDegrees=task.time*6
+        angleRadians=angleDegrees*pi/180
+        self.pandaActor.setPos(10*sin(angleRadians), -10*cos(angleRadians), 3)
+        self.camera.setPos(self.pandaActor.getPos() - (0, 1.5, -.75))
         self.camera.lookAt(self.pandaActor)
+
         return Task.cont
 
 app=MyApp()
