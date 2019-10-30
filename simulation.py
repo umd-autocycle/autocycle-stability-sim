0faf65a179b74dd7d4d06ea6d61672ba099e81e9
Jack Mirenzi <jmirenzi@umd.edu>
2019-10-29 23:15:23 -0400

    ode solver

--- simulation.py
+++ simulation.py
@@ -4,10 +4,14 @@ import matplotlib.pyplot as plt
 from sympy import *
 import sympy as sp
 
-M = np.array([[1, 2], [3, 4]])
-C = np.array([[1, 2], [3, 4]])
-K = np.array([[1, 2], [3, 4]])
-
+'''m11, m12, m21, m22, c11, c12, c21, c22, k11, k12, k21, k22 = sp.symbols('m11 m12 m21 m22 c11 c12 c21 c22 k11 k12 k21 k22')
+M = sp.Matrix([[m11, m12], [m21, m22]])
+C = sp.Matrix([[c11, c12], [c21, c22]])
+K = sp.Matrix([[k11, k12], [k21, k22]])
+'''
+M = sp.Matrix([[1, 2], [3, 4]])
+C = sp.Matrix([[1, 2], [3, 4]])
+K = sp.Matrix([[1, 2], [3, 4]])
 
 '''from scipy.integrate import solve_ivp
 def rhs(s, v):
@@ -19,14 +23,16 @@ print(res.y)'''
 t = sp.symbols('t')
 y0 = Function('y0')(t)
 y1 = Function('y1')(t)
-y = np.array([y0, y1])
-dy = np.array([y0.diff(t), y1.diff(t)])
-d2y = np.array([y0.diff(t,2), y1.diff(t,2)])
+y = sp.Matrix([y0, y1])
+dy = sp.Matrix([y0.diff(t), y1.diff(t)])
+d2y = sp.Matrix([y0.diff(t,2), y1.diff(t,2)])
+
 
 diffeq = M*d2y+C*dy+K*y
-print(Eq(diffeq[0][0], 0))
-print(Eq(diffeq[1][0], 0))
-print(dsolve([Eq(diffeq[0][0], 0), Eq(diffeq[1][0], 0)],y))
+print(Eq(diffeq[0], 0))
+print(Eq(diffeq[1], 0))
+print([y[0], y[1]])
+print(dsolve([Eq(diffeq[0], 0), Eq(diffeq[1], 0)], [y0, y1]))
 
 '''
 x = sp.Function('x')(t)
