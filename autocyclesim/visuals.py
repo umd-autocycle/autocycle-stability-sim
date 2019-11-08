2abbcba1b25739fa8b72c4881bcb0c138063e800
Cooper Grill <coopergrill@gmail.com>
2019-11-07 21:15:30 -0500

    add file

--- autocyclesim/visuals.py
+++ autocyclesim/visuals.py
@@ -12,23 +12,24 @@ class MyApp(ShowBase):
         ShowBase.__init__(self)
 
         #load environment
-        self.scene=self.loader.loadModel("models/environment")
+        self.scene=self.loader.loadModel("../Users/Cooper Grill/Documents/Autocycle/untitled.egg")
+        self.scene.setHpr(0, 180, 0)
         #reparent model to render
         self.scene.reparentTo(self.render)
         #apply scale and position
-        self.scene.setScale(0.25, 0.25, 0.25)
+        self.scene.setScale(100, 100, 100)
         self.scene.setPos(-8, 42, 0)
 
         # load and transform panda actor
         self.pandaActor = Actor("models/panda-model", {"walk": "models/panda-walk4"})
-        self.pandaActor.setScale(0.00005, 0.0005, 0.0005)
+        self.pandaActor.setScale(0.001, 0.01, 0.01)
         self.pandaActor.reparentTo(self.render)
         # loop animation
         self.pandaActor.loop("walk")
 
         # create intervals needed for panda to walk
-        self.pandaActor.setHpr(180, 0, 0)
-        self.pandaActor.setPos(0, -10, 3)
+        self.pandaActor.setHpr(180, 0, 1)
+        self.pandaActor.setPos(0, 0, 0)
         self.taskMgr.add(self.pandaTask, "PandaTask")
 
 
@@ -36,8 +37,8 @@ class MyApp(ShowBase):
     def pandaTask(self, task):
         angleDegrees=task.time*6
         angleRadians=angleDegrees*pi/180
-        self.pandaActor.setPos(10*sin(angleRadians), -10*cos(angleRadians), 3)
-        self.camera.setPos(self.pandaActor.getPos() - (0, 1.5, -.75))
+        self.pandaActor.setPos(10*sin(angleRadians), -10*cos(angleRadians), 1)
+        self.camera.setPos(self.pandaActor.getPos() - (0, 30, -15))
         self.camera.lookAt(self.pandaActor)
 
         return Task.cont
