edf5b3917b5730493f2708a604daa4a637306fc8
Evan Ruderman <evanrman@comcast.net>
2019-11-12 21:41:39 -0500

    Implemented capability to incorporate horizontal or vertical lines into the graph. Format for tuple: (int or float, name of line, horizontal or vertical defined by "h" or "v")

--- autocyclesim/graphs.py
+++ autocyclesim/graphs.py
@@ -7,8 +7,18 @@ def plot_params(title, xtuple, *data, dependent=""):
     matplotlib.rcParams['lines.linewidth'] = 2.5
     matplotlib.rc('font', family='Arial')
 
+    n = 0
     for datum in data:
-        plt.plot(xtuple[0], datum[0])
+        if isinstance(datum[0], list):
+            plt.plot(xtuple[0], datum[0])
+        elif type(datum[0]) in (float, int):
+            if datum[2] == "h":
+                plt.axhline(y=datum[0], color = colorlist[n])
+                n+=1
+            elif datum[2] == "v":
+                plt.axvline(x=datum[0], color = colorlist[n])
+                n+=1
+    n=0
     plt.legend([datum[1] for datum in data], fontsize=14)
     plt.xlabel(xtuple[1], fontsize=14)
     plt.ylabel(dependent, fontsize=14)
@@ -32,8 +42,19 @@ def generate_figure(title, xtuple, *data, dependent=""):
     matplotlib.rcParams['lines.linewidth'] = 2.5
     matplotlib.rc('font', family='Arial')
 
+    colorlist = ["black", "red", "green", "yellow"]
+    n = 0
     for datum in data:
-        plt.plot(xtuple[0], datum[0], figure = fig)
+        if isinstance(datum[0], list):
+            plt.plot(xtuple[0], datum[0], figure = fig)
+        elif type(datum[0]) in (float, int):
+            if datum[2] == "h":
+                plt.axhline(y=datum[0], color = colorlist[n], figure = fig)
+                n+=1
+            elif datum[2] == "v":
+                plt.axvline(x=datum[0], color = colorlist[n], figure = fig)
+                n+=1
+    n=0
     plt.legend([datum[1] for datum in data], fontsize=14)
     plt.xlabel(xtuple[1], fontsize=14, figure = fig)
     plt.ylabel(dependent, fontsize=14, figure = fig)
