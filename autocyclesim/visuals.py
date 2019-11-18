64aa1c36e7b90d4996ac585e8064fa3301a98993
Cooper Grill <coopergrill@gmail.com>
2019-11-18 12:04:25 -0500

    add file

--- autocyclesim/visuals.py
+++ autocyclesim/visuals.py
@@ -4,6 +4,8 @@ from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 from direct.actor.Actor import Actor
 
+# from simulation import simulate
+
 
 class MyApp(ShowBase):
     def __init__(self):
@@ -19,22 +21,19 @@ class MyApp(ShowBase):
 
         # load and transform bike actor
         self.bike = Actor("../Users/Cooper Grill/Documents/Autocycle/bike.egg")
-        self.bike.setColor(0, 0, 0, 0)
+        self.bike.setColor(0, 0, 0)
         self.bike.setHpr(180, 0, 0)
         self.bike.setPos(0, 0, 1)
         self.bike.reparentTo(self.render)
 
         # move bike and follow with camera
         self.taskMgr.add(self.bike_task, "bikeTask")
+        self.taskMgr.add(self.camera_task, "cameraTask")
 
-    # define procedure to move panda
+    # define procedure to move bike
     def bike_task(self, task):
         # set bike's velocity vector
         self.bike.setPos(self.bike.getPos() + (0, 1, 0))
-
-        # set camera
-        self.camera.setPos(self.bike.getPos() + (-30*sin(pi*self.bike.getHpr()[0]/180), 30*cos(pi*self.bike.getHpr()[0]/180), 10))
-        self.camera.lookAt(self.bike)
 
         # teleport bike based on position and angle
         if self.bike.getY() == 110 and 90 < self.bike.getHpr()[0] % 360 < 270: self.bike.setPos(0, -200, 1)
@@ -44,6 +43,12 @@ class MyApp(ShowBase):
 
         return Task.cont
 
+    # define task to move camera
+    def camera_task(self, task):
+        self.camera.setPos(self.bike.getPos() + (-30 * sin(pi * self.bike.getHpr()[0] / 180), 30 * cos(pi * self.bike.getHpr()[0] / 180), 10))
+        self.camera.lookAt(self.bike)
+
+        return Task.cont
 
 app = MyApp()
 app.run()
