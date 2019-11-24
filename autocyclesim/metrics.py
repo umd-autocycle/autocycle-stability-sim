72c8162715c3d57762d0f0db0205517d7d293157
Mikhail Khrenov <mkhrenov34@gmail.com>
2019-11-24 10:56:41 -0500

    Buffed up simulation a bit, finished initial metrics

--- autocyclesim/metrics.py
+++ autocyclesim/metrics.py
@@ -1,8 +1,12 @@
 from math import inf as infinity
 
 
+def settling_threshold(time, variable, goal):
+    return abs(variable[0] - goal) * 0.02
+
+
 def settling_time(time, variable, goal):
-    threshold = abs(variable[0] - goal)
+    threshold = settling_threshold(time, variable, goal)
 
     if not settles(time, variable, goal):
         return infinity
@@ -18,6 +22,25 @@ def settles(time, variable, goal):
     """
     does not account well for oscillating behavior, need more sophisticated approach
     """
-    threshold = abs(variable[0] - goal)
+    threshold = settling_threshold(time, variable, goal)
     return abs(variable[-1] - goal) >= threshold
 
+
+def overshoot(time, variable, goal):
+    if variable[0] < goal:
+        max_over = variable[0] - goal
+
+        for v in variable:
+            if v > goal and v - goal < max_over:
+                return max_over
+            else:
+                max_over = v - goal
+
+    else:
+        max_over = goal - variable[0]
+
+        for v in variable:
+            if v < goal and goal - v < max_over:
+                return max_over
+            else:
+                max_over = goal - v
