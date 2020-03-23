ddd5b94f4fc960485ae764bfc7b1b8bd70bfa650
Jack Mirenzi <jmirenzi717@gmail.com>
2020-03-23 17:06:05 -0400

    optimizer try 4

--- autocyclesim/optimizer.py
+++ autocyclesim/optimizer.py
@@ -14,6 +14,8 @@ def optimize(intial_val, vel, tspan, intial_constants, c_robust, c_response, max
     def time(x):
         results = func(x)
         con = float(metrics.settling_time(results['t'], results['phi'], 0))
+        if con > tspan:
+            con = (tspan + abs(metrics.overshoot(results['t'], results['phi'], 0)[0])) * c_response
         print('time', con)
         return con
 
@@ -37,7 +39,7 @@ def optimize(intial_val, vel, tspan, intial_constants, c_robust, c_response, max
 
     def objective(x):
         print('iteration:', x)
-        time_obj = (time(x) - max_response) ** 2
+        time_obj = (time(x) - max_response)
         # robust_obj = (robust(x) - min_robust) ** 2
         robust_obj = 0
         tor = torque(x)
@@ -51,9 +53,10 @@ def optimize(intial_val, vel, tspan, intial_constants, c_robust, c_response, max
 
 
 if __name__ == '__main__':
-    test = optimize(intial_val=[10, 0, 0, 0], vel=10, tspan=30,
-                    intial_constants=[1.19326220e+00, -6.49562030e-06, 1.31230247e+00], c_robust=1, c_response=50,
-                    max_torque=20, max_response=0, min_robust=10)
+    test = optimize(intial_val=[10, 0, 0, 0], vel=5.5, tspan=30,
+                    intial_constants=[ 0, 0,  0], c_robust=1, c_response=1e2,
+                    max_torque= 20, max_response=0, min_robust=10)
     print(test)
-    # 5.5 [ 3.44077315e+00 -2.23763598e-06  8.66977482e-02]
-    # 10  [ 1.19326220e+00 -6.49562030e-06  1.31230247e+00]
+    # 5.5 4.34s [ 3.44077315e+00, -2.23763598e-06,  8.66977482e-02] then @100 3.12s [ 8.38025928e+00, -1.32098845e-06,  3.49506937e+00]
+    # 10  29s [ 1.19326220e+00, -6.49562030e-06,  1.31230247e+00] then @100 t 3.36s [ 9.76156844e+00, -9.45659175e-07, -4.11625537e-01]
+    # 11 29.72s [ 1.31204884e+00, -6.51253072e-06,  1.31193448e+00] then @100t 3.84s [ 9.78287177e+00, -8.90521735e-07, -4.15691395e-01]
